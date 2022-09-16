c46776d125ecc9592dcb5a36d2f92a51f4a1ce09
Salifu Boniface Ugbedeojo <111025575+bonzill@users.noreply.github.com>
2022-09-16 23:31:55 +0100

    edited

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,15 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-#endif
+#include "main.h"
+/**
+ * _isupper - function
+ *
+ * @c: parameter
+ *
+ * Return: return value
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
